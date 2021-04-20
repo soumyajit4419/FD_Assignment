d1f422b15ff41ed888d02a856c275db7961b467f
soumyajit4419 <ashish454570@gmail.com>
2021-04-20 08:55:46 +0530

    fixed state erros in table page

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,33 +1,59 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { Col, Row, Container, Table, Button } from "react-bootstrap";
 import NavbarContent from "./Navbar";
-import { AppContext } from "../App";
+import axios from "axios";
 import { GrFormPreviousLink } from "react-icons/gr";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function TableDisplay() {
   const [details, setDetails] = useState([]);
+  const [appDetails, setAppDetails] = useState([]);
   const [appName, setAppName] = useState("");
   const [companyName, setCompanyName] = useState("");
-  const appStats = useContext(AppContext);
-  const stats = appStats.stats;
-  const comp = appStats.details;
   const history = useHistory();
   const params = useParams();
 
   useEffect(() => {
-    if (stats) {
-      setDetails(stats[params.id]);
+    let mounted = true;
+    const id = params.id;
+
+    axios
+      .get(`https://api.npoint.io/baf8dba5974d29aa094b/${id}`)
+      .then((res) => {
+        if (mounted) {
+          setDetails(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get("https://api.npoint.io/adf6676a313fa01f787d")
+      .then((res) => {
+        if (mounted) {
+          setAppDetails(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    if (appDetails) {
+      for (var i = 0; i < appDetails.length; i++) {
+        if (appDetails[i].id === id) {
+          setAppName(appDetails[i].appName);
+          setCompanyName(appDetails[i].publisherName);
+        }
+      }
     }
-    if (comp) {
-      setAppName(comp[params.id - 1].appName);
-      setCompanyName(comp[params.id - 1].publisherName);
-    }
-  }, [params.id, stats, comp]);
+
+    return () => (mounted = false);
+  }, [params.id, appDetails]);
 
   const handelClick = () => {
-    history.push("/");
+    history.goBack();
   };
 
   return (
